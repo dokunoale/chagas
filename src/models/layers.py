034dbf5528a34550defabe7b07dab21a9255ca67
dokunoale <153988414+dokunoale@users.noreply.github.com>
2025-07-13 17:42:55 +0200

    removing unused files

--- src/models/layers.py
+++ src/models/layers.py
@@ -11,39 +11,6 @@ class TemporalMeanPooling(tf.keras.layers.Layer):
     
 
 @keras.saving.register_keras_serializable()
-class LogSpectrogram(tf.keras.layers.Layer):
-    def __init__(self, frame_length=64, frame_step=32, fft_length=64, scale=1000.0, **kwargs):
-        super(LogSpectrogram, self).__init__(**kwargs)
-        self.frame_length = frame_length
-        self.frame_step = frame_step
-        self.fft_length = fft_length
-        self.scale = scale
-
-    def call(self, inputs):
-        spectrograms = []
-        for i in range(inputs.shape[-1]):  # For each lead
-            signal = inputs[..., i]
-            stft = tf.signal.stft(
-                signal,
-                frame_length=self.frame_length,
-                frame_step=self.frame_step,
-                fft_length=self.fft_length,
-                window_fn=tf.signal.hann_window,
-                pad_end=True
-            )
-            spectrogram = tf.abs(stft)
-            log_spectrogram = tf.math.log1p(self.scale * spectrogram)
-            spectrograms.append(log_spectrogram)
-
-        return tf.stack(spectrograms, axis=-1)
-
-    def compute_output_shape(self, input_shape):
-        time_dim = (input_shape[1] + self.frame_step - 1) // self.frame_step
-        freq_dim = self.fft_length // 2 + 1
-        return (input_shape[0], time_dim, freq_dim, 12)
-    
-
-@keras.saving.register_keras_serializable()
 class LightLogSpectrogram(tf.keras.layers.Layer):
     def __init__(self, frame_length=64, frame_step=32, fft_length=256,
                  scale=128.0, num_log_bins=64, **kwargs):
